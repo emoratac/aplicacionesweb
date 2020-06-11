e88b0241d2326b9f2a033351bd0a3a2300d90436
emoratac <atarom1987@gmail.com>
2020-06-11 16:32:29 +0200

    foto

--- P3/my_groupvlogamer/include/functions.php
+++ P3/my_groupvlogamer/include/functions.php
@@ -108,7 +108,7 @@ function MP_my_datos_vlogamer()
             
             
             $fotoURL="";
-            $IMAGENES_USUARIOS = '../fotos/';
+            $IMAGENES_USUARIOS = '/fotos/';
             if(array_key_exists('foto', $_FILES) && $_POST['email']) {
                 $fotoURL = $IMAGENES_USUARIOS.$_POST['userName']."_".$_FILES['foto']['name'];
                 if (move_uploaded_file($_FILES['foto']['tmp_name'], $fotoURL))
