738223843a92e16d8792dcf53f378a75c4bc8c15
emoratac <atarom1987@gmail.com>
2020-06-15 00:13:29 +0200

    xscxzcxzc

--- P3/my_groupvlogamer/include/functions.php
+++ P3/my_groupvlogamer/include/functions.php
@@ -66,71 +66,71 @@ function MP_Register_Form_vlogamer($MP_user , $user_email)
     </form>
 
     <script type="text/javascript" charset="utf-8" async defer >
-        // wp_enqueue_script('myScript');
+        wp_enqueue_script('myScript');
 
-        console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        // console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 
-        //carga la imagen de file en el elemento src imagen
-        function mostrarFoto(file, imagen) {
-        var reader = new FileReader();
-        reader.addEventListener("load", function () {
-            imagen.src = reader.result;
-        });
-        reader.readAsDataURL(file);
-        }
-
-        // envia formulario
-        async function registrarAsync(evento) {
-        try {
-            evento.preventDefault();
-            console.log("registrarAsync 1");
-            let url = evento.target.getAttribute("action");
-            let data = new FormData(evento.target);
-            let init = {
-            url: url,
-            method: "post",
-            body: data,
-            };
-            console.log("registrarAsync 2", url, data);
-
-            let request0 = new Request(url, init);
-
-            const response = await fetch(request0);
-
-            if (!response.ok) {
-            throw Error(response.statusText);
-            }
-            const result = await response.text();
-            alert("El registro se ha guardado correctamente.");
-            console.log("Correcto devuelvo:", result);
-        } catch (error) {
-            console.log(error);
-            alert("Hubo un error al guardar el registro. Inténtelo de nuevo.");
-        }
-        }
-
-        //escuchamos evento selección nuevo fichero.
-        function ready() {
-        console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-        var fichero = document.querySelector("#foto");
-        var imagen = document.querySelector("#img_foto");
-        fichero.addEventListener("change", function (event) {
-            mostrarFoto(this.files[0], imagen);
-        });
-
-        var myForm = document.querySelector("#myFormAsync");
-        myForm.addEventListener("submit", function (event) {
-            console.log("aded submit event");
-            registrarAsync(event);
-        });
-
-        // if (document.forms.length > 0) {
-        // document.forms[0].addEventListener("submit", function (event) {
-        //     enviaForm(event);
-        // })
+        // //carga la imagen de file en el elemento src imagen
+        // function mostrarFoto(file, imagen) {
+        // var reader = new FileReader();
+        // reader.addEventListener("load", function () {
+        //     imagen.src = reader.result;
+        // });
+        // reader.readAsDataURL(file);
         // }
-        }
-        ready();
+
+        // // envia formulario
+        // async function registrarAsync(evento) {
+        // try {
+        //     evento.preventDefault();
+        //     console.log("registrarAsync 1");
+        //     let url = evento.target.getAttribute("action");
+        //     let data = new FormData(evento.target);
+        //     let init = {
+        //     url: url,
+        //     method: "post",
+        //     body: data,
+        //     };
+        //     console.log("registrarAsync 2", url, data);
+
+        //     let request0 = new Request(url, init);
+
+        //     const response = await fetch(request0);
+
+        //     if (!response.ok) {
+        //     throw Error(response.statusText);
+        //     }
+        //     const result = await response.text();
+        //     alert("El registro se ha guardado correctamente.");
+        //     console.log("Correcto devuelvo:", result);
+        // } catch (error) {
+        //     console.log(error);
+        //     alert("Hubo un error al guardar el registro. Inténtelo de nuevo.");
+        // }
+        // }
+
+        // //escuchamos evento selección nuevo fichero.
+        // function ready() {
+        // console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+        // var fichero = document.querySelector("#foto");
+        // var imagen = document.querySelector("#img_foto");
+        // fichero.addEventListener("change", function (event) {
+        //     mostrarFoto(this.files[0], imagen);
+        // });
+
+        // var myForm = document.querySelector("#myFormAsync");
+        // myForm.addEventListener("submit", function (event) {
+        //     console.log("aded submit event");
+        //     registrarAsync(event);
+        // });
+
+        // // if (document.forms.length > 0) {
+        // // document.forms[0].addEventListener("submit", function (event) {
+        // //     enviaForm(event);
+        // // })
+        // // }
+        // }
+        // ready();
 
     </script>
 
@@ -147,7 +147,7 @@ function MP_my_datos_vlogamer()
 { 
     global $user_ID , $user_email,$table;
 
-    // wp_register_script('myScript', './actions.js' );
+    wp_register_script('myScript', './actions.js' );
     
     $MP_pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD); 
     wp_get_current_user();
